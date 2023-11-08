2df05a42cc8cc96fe120dc5a4e59303f00f39039
KorolikD <96023073+KorolikD@users.noreply.github.com>
2023-11-08 19:54:16 +0000

    Deploying to gh-pages from @ KorolikD/goit-react-hw-03-phonebook@28f178ec81fcd573282c01e4f96129514ca4f744 🚀

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export * from 'components/App/App';
-
-export * from 'components/ContactForm/ContactForm';
-
-export * from 'components/ContactsList/ContactsList';
-
-export * from 'components/Filter/Filter';
